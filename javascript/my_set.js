b062bb39df4d47f0b7a6be6868de3e9a9afccfc6
RyanDeLong <rdelong111@gmail.com>
2022-03-09 21:46:18 -0600

    commit tree ca3c604e47e9261b4e4529f84cee5e371e0877e4

--- javascript/my_set.js
+++ javascript/my_set.js
@@ -4,37 +4,63 @@ class MySet {
   // strings and arrays will need to be broken down by their elements/characters
   constructor(iterable) {
     this.data = {};
+    if (iterable) {
+      if (Array.isArray(iterable) || typeof iterable === 'string') {
+        for (const single of iterable) {
+          if (!(single in this.data)) {
+            this.data[single] = true;
+          }
+        }
+      }
+      else {
+        throw Error('Error');
+      }
+    }
   }
 
   // return number of elements in MySet
   size() {
-
+    return Object.keys(this.data).length;
   }
 
   // add an item to MySet as is
   // don't worry about arrays here!
   // return the MySet instance
   add(item) {
-
+    if (!(item in this.data)) {
+      this.data[item] = true;
+    }
+    return this;
   }
 
   // delete an item from MySet
   // don't worry about arrays here!
   // return true if successful, otherwise false
   delete(item) {
-
+    if (item in this.data) {
+      delete this.data[item];
+      return true;
+    }
+    else {
+      return false;
+    }
   }
 
   // return true if in MySet, otherwise false
   // don't worry about arrays here!
   has(item) {
-
+    if (item in this.data) {
+      return true;
+    }
+    else {
+      return false;
+    }
   }
 
   // return data as an array
   // don't worry about arrays here!
   entries() {
-
+    return Object.keys(this.data);
   }
 }
 
